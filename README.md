14c6e07f3b03e5ee77a0e85242ff7c1585545089
Guilherme Gabriel <guilhermag@gmail.com>
2022-05-11 07:02:19 -0300

    finalizacao do projeto

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto Banco Digital com Java
+
+O objetivo desse projeto foi colocar em prática os conhecimentos adquiridos durante o curso da [Digital Innovation One](https://www.dio.me/).
+
+## Tecnologias utilizadas
+
+- Java.
+
+## Como testar o projeto
+
+### Clone o repositório e abra remotamente
+
+Digite no seu terminal
+
+``
+git clone https://github.com/guilhermag/dio-banco-digital-java.git
+``
+
+``
+cd dio-banco-digital-java/
+``
+
+Agora é só rodar com a sua JDK/JRE de qualquer versão superior à Java 8.
+
+
+## Créditos
+
+Desenvolvido por Guilherme de Araujo Gabriel.
--- src/Conta.java
+++ src/Conta.java
@@ -14,16 +14,31 @@ public class Conta implements IConta {
 
     @Override
     public void sacar(double valor) {
-
+        if ( valor > this.saldo) {
+            System.out.println("Saldo insuficiente!");
+            return;
+        }
+        this.saldo -= valor;
     }
 
     @Override
     public void depositar(double valor) {
-
+        this.saldo += valor;
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        if (this.saldo < valor) {
+            System.out.println("Saldo insuficiente para transferência!");
+            return;
+        }
+        contaDestino.depositar(valor);
+    }
 
+    protected void imprimirInformacoes() {
+        System.out.printf("Agência: %d%n", this.agencia);
+        System.out.printf("Número: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
     }
 }
